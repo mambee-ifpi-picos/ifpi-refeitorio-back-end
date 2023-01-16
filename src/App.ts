e1e6fcef8ba0c925e86330cb858513263019272e
gabrielsszz <gabrielfndsts265@gmail.com  git config --global user.name Your>
2023-01-16 17:19:16 -0300

    feat: starting documentation with swagger

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import helmet from 'helmet';
 import routes from './routes/Index';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json'
 
 // criação de uma instância do express/servidor http
 const app = express();
@@ -8,6 +10,9 @@ app.use(helmet()); // https://helmetjs.github.io/
 
 // middleware para converter o body das requisições para json
 app.use(express.json());
+
+// documentação com o swagger 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // middleware de tratamento de todas as rotas
 app.use('/', routes);
